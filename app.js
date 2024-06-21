3a57fd0d32eae34943e6cfb1a61f37a9f7eb5d39
Antonioc-98 <antonio.cut1234@gmail.com>
2024-06-21 22:53:38 +0200

    Podatci iz POST requesta za registraciju se zapisuju u bazu podataka. Prije toga se provjerava ako je e-mail duplikat. Rezultat uspjesne ili neuspjesne registracije se vraca na stranici register_status.ejs.

--- app.js
+++ app.js
@@ -20,27 +20,50 @@ const db = mysql.createConnection({
 
 db.connect((err) => {
     if (err) {
-        console.log('Error on connection')
-    }
-    console.log('MySql connected...')
-})
+        console.log('Error on connection');
+    };
+    console.log('MySql connected...');
+});
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', {proba: 'Radi'})
-})
+    res.render('index.ejs', {proba: 'Radi'});
+});
 
 app.get('/register', (req, res) => {
-    res.render('register.ejs')
-})
+    res.render('register.ejs');
+});
+
+app.get('/register_status', (req, res) => {
+    res.render('register_status.ejs');
+});
+
+//Provjera
 
 app.post('/register', (req, res) => {
-    let user = {ime: req.body.name, email: req.body.email, lozinka: req.body.password};
-    console.log(user);
-    res.send(`Registracija uspješna ${req.body.name} ${req.body.email} ${req.body.password}`);
-})
+
+    let user = {ime: req.body.first_name, prezime: req.body.last_name, email: req.body.email, lozinkaHashed: req.body.password}
+    let registerStatus = ''
+
+    db.query(`SELECT email FROM users WHERE email = '${req.body.email}'`, (error, result) => {
+        if (error) throw error;
+        if(result.length != 0) {
+            console.log('Duplikat!');
+            registerStatus = 'E-mail je već iskorišten za registraciju. Upišite novi email ili se pokušajte prijaviti.'
+        } else {
+            console.log('Provjera = nema duplikata')
+            registerStatus = 'Registracija uspješna';
+            let sql = 'INSERT INTO users SET ?';
+            db.query(sql, user, (error, result) => {
+                if (error) throw error;
+                console.log(result);     
+            });
+        };
+        res.render('register_status.ejs', {resolved: registerStatus});
+    });
+});
 
 
 
 app.listen('3000', () => {
-    console.log('Server started on port 3000...')
-})
+    console.log('Server started on port 3000...');
+});
